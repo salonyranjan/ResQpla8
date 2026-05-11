d37b7ad5741247dad366eba1031ab3e08251f7ac
Salony Ranjan <salonyranjan@gmail.com>
2026-05-12 02:29:03 +0530

    Reorder imports and add context providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import "./App.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+
+// Context & State Providers
 import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
+
+// Styles
+import "./index.css";
+import "./App.css";
 import "leaflet/dist/leaflet.css";
+
+// Components
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
